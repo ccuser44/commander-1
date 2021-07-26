0fe26c3cee6121270c79e6115b98ba37d0628139
7kayoh <40730127+7kayoh@users.noreply.github.com>
2021-07-26 21:55:44 +0800

    added BanUtil

---
+++ src/Packages/Plugins/APIExtensions/AdminExtenders/PlayerWrapper/BanUtil.lua
@@ -0,0 +1,20 @@
+-- TODO: finish datastore
+
+local package = {
+    Name = "BanUtil",
+    Description = "A plugin that adds a ban system to Commander, meant to be used with the command",
+    Author = "7kayoh",
+    Class = "Plugin",
+    Target = {}
+}
+
+function package.Target.extenderFunction(player, wrapper)
+    
+end
+
+function package.Target:Init()
+    package.Settings = require(package.Core.Settings)
+    package.API.extendPlayerWrapper(package.Target.extenderFunction)
+end
+
+return package
