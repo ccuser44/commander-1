384074fb892ea3372ee76380015685371c3be4ba
7kayoh <40730127+7kayoh@users.noreply.github.com>
2021-07-31 08:45:29 +0800

    added UIKit class

---
+++ src/MainModule/Core/Injectables/UIKit/init.lua
@@ -0,0 +1,18 @@
+--[[
+    UIKit.lua
+    ==========
+
+    The server wrapper for the UI framework. For Commander packages to
+    communicate with the UI framework such as creating new components, et
+    cetera.
+
+    Refer to the documentation for more information on how to use this wrapper.
+
+    TODO
+]]
+
+local UIKit = {}
+
+
+
+return UIKit
