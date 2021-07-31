701483a10d7e7d2d6b7eb0c4bb32ae3423501702
7kayoh <40730127+7kayoh@users.noreply.github.com>
2021-07-31 08:45:39 +0800

    added API methods extension

---
+++ src/Packages/Plugins/APIExtensions/RemoteTasks/API.lua
@@ -0,0 +1,28 @@
+local package = {
+    Name = "API",
+    Description = "Reveals specific API methods to remote",
+    Author = "7kayoh",
+    Class = "Plugin",
+    Target = {}
+}
+local allowedRequests = {
+    "getAdminLevel",
+    "getAdminStatusWithUserId",
+    "checkUserAdmin",
+}
+
+local dLog = nil
+
+function package.Target.remoteTask(player, requestType, ...)
+    dLog("Info", player.Name .. " is currently calling API method " .. requestType)
+    return package.API[requestType](...)
+end
+
+function package.Target:Init()
+    dLog = require(package.Core.dLog)
+    package.API.addRemoteTask("Function", function(player, requestType)
+        return package.API.checkUserAdmin(player) and table.find(allowedRequests, requestType) ~= nil
+    end, package.Target.remoteTask)
+end
+
+return package
