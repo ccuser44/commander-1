2ad144dac62a4e5fbb3d552bbb9d9bb8946cd022
7kayoh <40730127+7kayoh@users.noreply.github.com>
2021-08-14 10:17:28 +0800

    Added MagicTools

---
+++ src/MainModule/Core/Injectables/MagicTools/PackColor3.lua
@@ -0,0 +1,7 @@
+return function(color3: Color3): {R: number, G: number, B: number}
+    return {
+        R = color3.R,
+        G = color3.G,
+        B = color3.B
+    }
+end
---
+++ src/MainModule/Core/Injectables/MagicTools/init.lua
@@ -0,0 +1,5 @@
+local strict = require(script.Parent.Parent.strict)
+
+return strict {
+    ["packColor3"] = require(script.PackColor3) 
+}
