a1c95bebf864166df4f1a4d50de392bc99e7e5e5
7kayoh <40730127+7kayoh@users.noreply.github.com>
2021-08-25 10:57:40 +0800

    Added Shared assets

---
+++ src/MainModule/Assets/Shared/UIKit/init.lua
@@ -0,0 +1,20 @@
+-- 7kayoh
+-- UIKit.lua
+-- August 24, 2021
+
+-- Singletons
+local RunService = game:GetService("RunService")
+
+-- Private declarations
+local Components = script.Components
+local Shared = script.Parent
+
+local strictify = require(Shared.strictify)
+
+-- Runtime code
+assert(RunService:IsClient, "Can be only required when RunService:IsClient() is true")
+
+-- Returns
+return strictify {
+  ["Window"] = require(Components.Window)
+}
